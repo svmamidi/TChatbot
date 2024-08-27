b2c68e1c7cf3777e212b24c39e705e21fd3ea3c0
svm <svaniponnada@gmail.com>
2024-08-27 15:09:47 -0500

    final commit

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, jsonify, request
 from dotenv import load_dotenv
 import os
-# from tradingbot.retrieval_generation import generation
-# from tradingbot.data_ingestion import ingestdata
+from tradingbot.retrieval_generation import generation
+from tradingbot.data_ingestion import ingestdata
 
 app = Flask(__name__)
 
 load_dotenv()
 
-# vstore=ingestdata("done")
-# chain=generation(vstore)
+vstore=ingestdata("done")
+chain=generation(vstore)
 
 @app.route("/")
 def index():
---
+++ tradingbot/helper.py
@@ -0,0 +1,23 @@
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+def load_file():
+    loader = PyPDFLoader("C:\\Users\\svani\\OneDrive\\Documents\\Inueron-7-projects\\Trading-Chatbot\\data\\finance_data.pdf")
+    pages = loader.load()
+
+    raw_text = ''
+    for i, doc in enumerate(pages):
+        text = doc.page_content
+        if text:
+            raw_text += text
+            
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap  = 100,
+    )
+
+    docs = text_splitter.split_text(raw_text)
+    
+    return docs
+        
